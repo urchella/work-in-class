7326715ad9fa2306504c01ec407e771d75f05040
urchella <91685952+urchella@users.noreply.github.com>
2021-10-21 17:01:22 +0300

    test

---
+++ urchela.cpp
@@ -0,0 +1,40 @@
+﻿/*Шульга Юрий П43021*/
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int A;
+	cin >> A;
+	if (A == 1) { cout << "B mesece 31 den "; }
+	else if (A == 2) { cout << "B mesece 28 dnei "; }
+	else if (A == 3) { cout << "B mesece 31 dnei "; }
+	else if (A == 4) { cout << "B mesece 30 dnei "; }
+	else if (A == 5) { cout << "B mesece 31 dnei "; }
+	else if (A == 6 ){ cout << "B mesece 30 dnei "; }
+	else if (A == 7) { cout << "B mesece 31 dnei "; }
+	else if (A == 8) { cout << "B mesece 31 dnei "; }
+	else if (A == 9) { cout << "B mesece 30 dnei "; }
+	else if (A == 10){ cout << "B mesece 31 dnei "; }
+	else if (A == 11){ cout << "B mesece 30 dnei "; }
+	else if (A == 12){ cout << "B mesece 31 dnei "; }
+
+
+
+
+
+	
+	
+	
+	
+	
+
+}
+
+  
+
+
+
+
+
+
